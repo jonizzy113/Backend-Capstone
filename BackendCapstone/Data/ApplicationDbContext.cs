955500b2254f6feb948999abac579c68e9c6e98e
jonathan schaffer <jonizzyschaffer@gmail.com>
2019-07-25 12:02:31 -0500

    Initial setup

---
+++ BackendCapstone/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BackendCapstone.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendCapstone.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options){ }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
