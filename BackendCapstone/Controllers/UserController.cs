26ba0294f1eec2def7a236ac2fdb5d47d7058e60
jonathan schaffer <jonizzyschaffer@gmail.com>
2019-07-29 10:24:31 -0500

    Showing Brokers

---
+++ BackendCapstone/Controllers/UserController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BackendCapstone.Data;
+using BackendCapstone.Models;
+
+namespace BackendCapstone.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.UserType.ToListAsync());
+        }
+
+        public   async Task<IActionResult> GetSalesman()
+        {
+            var applicationUsers = await  _context.ApplicationUsers
+                .Include(u => u.UserType)
+                .Where(u => u.UserType.Type == "Salesman").ToListAsync();
+            return View(applicationUsers);
+        }
+        public async Task<IActionResult> GetBroker()
+        {
+            var applicationUsers = await _context.ApplicationUsers
+                .Include(u => u.UserType)
+                .Where(u => u.UserType.Type == "Broker").ToListAsync();
+            return View(applicationUsers);
+        }
+
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userType = await _context.UserType
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userType == null)
+            {
+                return NotFound();
+            }
+
+            return View(userType);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Type")] UserType userType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userType);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userType = await _context.UserType.FindAsync(id);
+            if (userType == null)
+            {
+                return NotFound();
+            }
+            return View(userType);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Type")] UserType userType)
+        {
+            if (id != userType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserTypeExists(userType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userType);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userType = await _context.UserType
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userType == null)
+            {
+                return NotFound();
+            }
+
+            return View(userType);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var userType = await _context.UserType.FindAsync(id);
+            _context.UserType.Remove(userType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserTypeExists(int id)
+        {
+            return _context.UserType.Any(e => e.Id == id);
+        }
+    }
+}
